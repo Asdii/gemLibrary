70f273cd9ad1f1ad7ce850a8bfe1a14a12cd3e2a
Asdii <joaquinherrera2016@gmail.com>
2025-03-06 02:57:13 -0300

    Change vite config

--- vite.config.js
+++ vite.config.js
@@ -7,8 +7,8 @@ export default defineConfig({
         viteStaticCopy({
             targets: [
                 {
-                    src: normalizePath(path.resolve(__dirname, './assets') + '/[!.]*'), // 1️⃣
-                    dest: normalizePath('./assets'), // 2️⃣
+                    src: normalizePath(path.resolve(__dirname, './gemLibrary/assets') + '/[!.]*'), // 1️⃣
+                    dest: normalizePath('./gemLibrary/assets'), // 2️⃣
                 },
             ],
         }),
