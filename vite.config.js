112735ea0634ef71080067359392c88b1009013f
Asdii <joaquinherrera2016@gmail.com>
2025-03-06 03:05:44 -0300

    Change vite config

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,13 @@ import { viteStaticCopy } from 'vite-plugin-static-copy'
 import path from 'path'
 
 export default defineConfig({
+    base: "gemLibrary",
     plugins: [
         viteStaticCopy({
             targets: [
                 {
-                    src: normalizePath(path.resolve(__dirname, './gemLibrary/assets') + '/[!.]*'), // 1️⃣
-                    dest: normalizePath('./gemLibrary/assets'), // 2️⃣
+                    src: normalizePath(path.resolve(__dirname, './assets') + '/[!.]*'), // 1️⃣
+                    dest: normalizePath('./assets'), // 2️⃣
                 },
             ],
         }),
